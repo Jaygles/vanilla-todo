eea3fb04599c92858a4c7bd5377276b3128311cf
Jay Sullivan <JaySull514@gmail.com>
2018-08-23 20:08:45 -0600

    First commit

---
+++ main.js
@@ -0,0 +1,34 @@
+function createTodo(text) {
+  const todoDiv = document.createElement('div');
+  todoDiv.className = 'todo';
+
+  const todoTextP = document.createElement('p');
+  const todoText = document.createTextNode(text);
+  todoTextP.appendChild(todoText);
+
+  todoDiv.appendChild(todoTextP);
+
+  const todoCheck = document.createElement('input');
+  todoCheck.type = 'checkbox';
+
+  todoCheck.addEventListener('click', function() {
+    if (todoDiv.classList.contains('checked')) {
+      todoDiv.classList.remove('checked');
+    } else {
+      todoDiv.classList.add('checked');
+    }
+  });
+
+  todoDiv.appendChild(todoCheck);
+
+  const todos = document.getElementById('todos');
+  todos.appendChild(todoDiv);
+}
+
+const createTodoBtn = document.getElementById('create-todo-btn');
+createTodoBtn.addEventListener('click', function(event) {
+  event.preventDefault();
+  let todoText = document.getElementById('todoText').value;
+  todoText += '';
+  createTodo(todoText);
+});
